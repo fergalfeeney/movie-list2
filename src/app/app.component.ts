ad329b2e79fe9ffecd09039616b1d2e90f99ad87
Fergal Feeney <feeneyfergal4@gmail.com>
2023-02-07 16:36:30 +0000

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movie-list2';
+
+  moviesList = [
+    {id: 1, title: 'The Godfather', year: '1972', director: 'Francis Ford Coppola'},
+    {id: 2, title: 'Millers Crossing', year: '1998', director: 'The Coen Brothers'},
+    {id: 3, title: 'Dial M for Murder', year: '1954', director: 'Alfred Hitchcock'},
+    {id: 4, title: 'Whiplash', year: '2014', director: 'Damien Chazelle'}
+  ];
 }
